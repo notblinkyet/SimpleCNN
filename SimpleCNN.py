b3685586999e349db19d078f56091e467daeef38
Yaroslav Chicherin <sneakerya@yandex.ru>
2024-02-10 22:45:23 +0300

    Первый коммит

---
+++ SimpleCNN.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+class SimpleCNN(nn.Module):
+  def __init__(self, n_classes):
+    super().__init__()
+    self.conv1 = nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2)
+    )
+    self.conv2 = nn.Sequential(
+        nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2)
+    )
+    self.conv3 = nn.Sequential(
+        nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2)
+    )
+    self.conv4 = nn.Sequential(
+        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2)
+    )
+    self.conv5 = nn.Sequential(
+        nn.Conv2d(in_channels=64, out_channels=96, kernel_size=3),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2)
+    )
+    self.flatten = nn.Flatten()
+
+    self.out = nn.Linear(96 * 5 * 5, n_classes)
+
+  def forward(self, x):
+    x = self.conv1(x)
+    x = self.conv2(x)
+    x = self.conv3(x)
+    x = self.conv4(x)
+    x = self.conv5(x)
+    x = self.flatten(x)
+    return self.out(x)
